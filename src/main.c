41fa77ea6d5ca303cd9478fbda7d9284f1dfbc7b
Yu Hou <hy1995@gmail.com>
2018-12-03 14:08:41 +1100

    fix

--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <sel4/sel4.h>
 
 int main(int argc, char const *argv[])
 {
-    printf("Here!");
+    // seL4_DebugPutChar('H');
+    // seL4_DebugPutChar('i');
+    // seL4_DebugPutChar('!');
+    // seL4_DebugPutChar('\n');
+    printf("TEST\n");
+
+    while(1);
     return 0;
 }
